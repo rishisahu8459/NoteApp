72ca2fbdc4177c20917f840c58308407ba07ac5a
rishisahu8459 <49360938+rishisahu8459@users.noreply.github.com>
2023-08-20 01:48:05 +0530

    is

--- Noteappp/src/NewNote.tsx
+++ Noteappp/src/NewNote.tsx
@@ -1,8 +1,15 @@
+import { NoteForm } from "./NoteForm"
 export function NewNote(){
     return (
+    <>
+    
+    <h1 className="mb-4">New Note</h1>
+    <NoteForm/>
     
     
-    <h1>hiiii</h1>
+    </>
+    
+    
     
     
     )
