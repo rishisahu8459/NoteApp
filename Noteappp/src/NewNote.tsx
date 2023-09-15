0a3a9a1e64247a8e4cf9ef371c64ae6707dcb0d6
rishisahu8459 <49360938+rishisahu8459@users.noreply.github.com>
2023-09-15 16:59:52 +0530

    final commit

--- Noteappp/src/NewNote.tsx
+++ Noteappp/src/NewNote.tsx
@@ -1,32 +1,21 @@
-import { NoteData} from "./App"
-import { Tag } from "./App"
+import { NoteData, Tag } from "./App"
 import { NoteForm } from "./NoteForm"
 
 type NewNoteProps = {
-    onSubmit: (data:NoteData)=> void
-    onAddTag: (tag: Tag) => void
-    availableTags: Tag[]
+  onSubmit: (data: NoteData) => void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
 
-
-
-
-export function NewNote({onSubmit, onAddTag, availableTags}:NewNoteProps){
-    return (
+export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
+  return (
     <>
-    
-    <h1 className="mb-4">New Note</h1>
-    <NoteForm 
-    onSubmit={onSubmit}
-    onAddTag= {onAddTag}
-    availableTags= {availableTags}
-     />
-    
-    
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
     </>
-    
-    
-    
-    
-    )
-    }
+  )
+}
